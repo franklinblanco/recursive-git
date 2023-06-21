e7786e3497fab3791bd8ed831f425c0d7f772391
Franklin <franklinblanco@tutanota.com>
2023-06-21 11:33:32 -0400

    Fixed commit bug

--- src/operations.rs
+++ src/operations.rs
@@ -7,12 +7,13 @@ pub fn pull(current_dir: PathBuf) {
 
 pub fn push(current_dir: PathBuf) {
     let (successes, errors) = recursive_command(current_dir, &mut Command::new("git").arg("push"));
-    println!("### recursive-git pull ran {} times, successes: {successes} | errors: {errors}", successes + errors);
+    println!("### recursive-git push ran {} times, successes: {successes} | errors: {errors}", successes + errors);
 }
 
 pub fn commit(current_dir: PathBuf, message: String) {
-    let (successes, errors) = recursive_command(current_dir, &mut Command::new("git").arg("commit").arg("-m").arg(format!("\"{message}\"")));
-    println!("### recursive-git pull ran {} times, successes: {successes} | errors: {errors}", successes + errors);
+    let (add_successes, add_errors) = recursive_command(current_dir.clone(), &mut Command::new("git").arg("add").arg("."));
+    let (commit_successes, commit_errors) = recursive_command(current_dir, &mut Command::new("git").arg("commit").arg("-m").arg(format!("\"{message}\"")));
+    println!("### recursive-git commit ran {} times, add: successes: {add_successes} | errors: {add_errors} | commit: successes: {commit_successes} | errors: {commit_errors}", add_successes + add_errors + commit_successes + commit_errors);
 }
 
 fn recursive_command(path: PathBuf, command: &mut Command) -> (i32, i32) {
